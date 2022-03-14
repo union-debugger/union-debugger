b7f125c57a9304d84c417de0fa8022289a718334
dssgabriel <dss.gabriel@protonmail.com>
2022-03-14 21:27:55 +0100

    Minor tweaks

---
+++ include/consts.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define BUFFER_LEN 256
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define BOLD "\033[1m"
+#define NORMAL "\033[0m"
