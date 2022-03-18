4c6ac9abf37787e816eead36a5f4b989767ab14b
dssgabriel <dss.gabriel@protonmail.com>
2022-03-18 01:36:36 +0100

    Streamlined config and cleaned up code to avoid memory leaks

--- include/config.h
+++ include/config.h
@@ -4,15 +4,13 @@
 
 typedef struct config_s {
     char* target;
-    char** args;
-    size_t nb_args;
-    i32 inferior_pid;
+    pid_t pid;
+    pid_t inferior_pid;
+    size_t inferior_start;
 } config_t;
 
-config_t* config_new(char* target, char** args, size_t nb_args);
-void config_drop(config_t *self);
-void config_set_target(config_t *self, char const* target);
-void config_set_args(config_t *self, char const* args);
+config_t* config_new(char* target);
+void config_drop(config_t* self);
+void config_load(config_t* self, char const* target);
 char const* config_target(config_t const* self);
-char* const* config_args(config_t const* self);
 void config_print(config_t const* self);
--- src/config.c
+++ src/config.c
@@ -1,90 +1,71 @@
+#include <elf.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "../include/config.h"
 #include "../include/utils.h"
 
-config_t *config_new(char* target, char** args, size_t nb_args)
+config_t *config_new(char* target)
 {
     config_t* cfg = malloc(sizeof(config_t));
     UD_assert(cfg, "config allocation failed");
     cfg->target = target;
-    cfg->args = args;
-    cfg->nb_args = nb_args;
+    cfg->pid = getpid();
     cfg->inferior_pid = -1;
+    cfg->inferior_start = 0;
     return cfg;
+}
+
+void config_clear(config_t *self)
+{
+    UD_assert(self, "invalid parameter (null pointer)");
+    free(self->target);
+    self->inferior_pid = -1;
+    self->inferior_start = 0;
 }
 
 void config_drop(config_t *self)
 {
     UD_assert(self, "invalid parameter (null pointer)");
-    if (self->args) {
-        for (size_t i = 0; i < self->nb_args; i++) {
-            printf("%s ", self->args[i]);
-            free(self->args[i]);
-        }
-    }
+    config_clear(self);
     free(self);
 }
 
-void config_set_target(config_t* self, char const* target)
+void config_load(config_t* self, char const* target)
 {
     UD_assert(self && target, "invalid parameter (null pointer)");
-    self->target = malloc(strlen(target) + 1);
+    if (self->target) {
+        printf("Executable has already been loaded\n");
+        return;
+    }
+    self->target = strdup(target);
     UD_assert(self->target, "config's target allocation failed");
-    self->target = strcpy(self->target, target);
-    UD_assert(self->target, "config's target string copy failed");
-    printf("%s%sinfo:%s target set to `%s`.\n", BOLD, MAGENTA, NORMAL, self->target);
-}
 
-void config_set_args(config_t* self, char const* args)
-{
-    UD_assert(self && args, "invalid parameter (null pointer)");
-    self->args = strsplit(args, " ");
-    self->nb_args = substr_cnt(args, " ");
-    printf("%s%sinfo:%s arguments set to [", BOLD, MAGENTA, NORMAL);
-    UD_assert(self && args, "invalid parameter (null pointer)");
-    for (size_t i = 1; i < self->nb_args + 1; i++) {
-        printf(" `%s`", self->args[i]);
-    }
-    printf(" ].\n");
-}
+    self->target = strdup(target);
+    UD_assert(self->target, "failed to duplicate target path");
 
-char const* config_target(config_t const* self)
-{
-    UD_assert(self, "invalid parameter (null pointer)");
-    if (!self->target) {
-        return NULL;
-    }
-    char* target = malloc(strlen(self->target) * sizeof(char));
-    UD_assert(target, "config target allocation failed");
-    target = strcpy(target, self->target);
-    UD_assert(target, "config target copy failed");
-    return target;
-}
+    FILE* fd = fopen(target, "rb");
+    UD_assert(fd, "failed to open target binary");
 
-char* const* config_args(config_t const* self)
-{
-    UD_assert(self, "invalid parameter (null pointer)");
-    if (!self->args) {
-        return NULL;
-    }
-    char** args = malloc(self->nb_args * sizeof(char*));
-    UD_assert(args, "config arguments list allocation failed");
-    for (size_t i = 0; i < self->nb_args + 1; i++) {
-        args[i] = malloc(strlen(self->args[i]) + 1);
-        UD_assert(args, "config argument allocation failed");
-        args[i] = strcpy(args[i], self->args[i]); 
-        UD_assert(args, "config arguments copy failed");
-    }
-    return args;
+    u8 elf_buf[sizeof(Elf64_Ehdr)];
+    size_t nread = fread(elf_buf, sizeof(Elf64_Ehdr), 1, fd);
+    fclose(fd);
+    UD_assert(nread == 1, "failed to read ELF header");
+
+    Elf64_Ehdr* elf_hdr = (Elf64_Ehdr*)(elf_buf);
+    UD_assert(elf_hdr->e_ident[EI_MAG0] == ELFMAG0 && elf_hdr->e_ident[EI_MAG1] == ELFMAG1 &&
+              elf_hdr->e_ident[EI_MAG2] == ELFMAG2 && elf_hdr->e_ident[EI_MAG3] == ELFMAG3 &&
+              elf_hdr->e_ident[EI_CLASS] == ELFCLASS64 && elf_hdr->e_machine == EM_X86_64,
+              "ELF is invalid");
+
+    self->inferior_start = elf_hdr->e_entry;
+    config_print(self);
 }
 
 void config_print(config_t const* self)
 {
     UD_assert(self, "invalid parameter (null pointer)");
-    printf("Current executable is set to `%s` with arguments: [", self->target);
-    for (size_t i = 1; i < self->nb_args + 1; i++) {
-        printf(" `%s`", self->args[i]);
-    }
-    printf(" ].\n");
+    printf("Current executable is set to `%s`, PID #%d (start address: %zu)\n",
+           self->target, self->inferior_pid, self->inferior_start);
 }
