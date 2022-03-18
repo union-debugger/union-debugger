514d7bdc80031cd72f5dcb5ebe1a9caa5d6733e8
dssgabriel <dss.gabriel@protonmail.com>
2022-03-18 01:35:19 +0100

    Minor fix

--- tests/hello.c
+++ tests/hello.c
@@ -3,7 +3,8 @@
 int main(int argc, char** argv)
 {
     if (argc < 2) {
-        printf("Not enought arguments\n");
+        printf("Not enough arguments\n");
+        return 1;
     }
     printf("Hello %s!\n", argv[1]);
     return 0;
