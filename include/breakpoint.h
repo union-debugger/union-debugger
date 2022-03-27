eb5ce5ac56d091182e961db5ef735bbeb993cbd3
dssgabriel <dss.gabriel@protonmail.com>
2022-03-27 01:21:47 +0100

    Better breakpoints!
    Breakpoints can now be set before the executable runs (gdb/lldb style) and are automatically enabled when the program is run for the first time

--- include/breakpoint.h
+++ include/breakpoint.h
@@ -3,14 +3,21 @@
 #include "config.h"
 #include "types.h"
 
+enum breakpoint_state {
+    BRK_UNINIT,
+    BRK_ENABLED,
+    BRK_DISABLED,
+};
+
 typedef struct breakpoint_t {
     size_t address;
     u64 original_data;
-    bool is_enabled;
+    enum breakpoint_state state;
 } breakpoint_t;
 
 i32 breakpoint_new(config_t* cfg, size_t const address);
-breakpoint_t* breakpoint_peek(vec_t* breakpoints, size_t const address);
+breakpoint_t const* breakpoint_get(vec_t const* breakpoints, size_t const address);
+i32 breakpoint_setup(config_t* cfg);
 i32 breakpoint_enable_id(config_t* cfg, size_t const id);
 i32 breakpoint_disable_id(config_t* cfg, size_t const id);
 void breakpoint_list(config_t const* cfg);
--- src/breakpoint.c
+++ src/breakpoint.c
@@ -9,14 +9,16 @@
 
 #define INT3 0xcc
 
+/**
+ * Internal functions.
+ **/
 static
-ssize_t breakpoint_search(vec_t* breakpoints, size_t const address)
+ssize_t breakpoint_search(vec_t const* breakpoints, size_t const address)
 {
-    breakpoint_t b;
     ssize_t found = -1;
     for (size_t i = 0; i < breakpoints->len; i++) {
-        b = *(breakpoint_t*)(vec_peek(breakpoints, i));
-        if (b.address == address) {
+        breakpoint_t const* b = vec_peek(breakpoints, i);
+        if (b->address == address) {
             found = i;
             break;
         }
@@ -24,36 +26,22 @@ ssize_t breakpoint_search(vec_t* breakpoints, size_t const address)
     return found != -1 ? found : -1;
 }
 
-breakpoint_t* breakpoint_peek(vec_t* breakpoints, size_t const address)
-{
-    breakpoint_t* b;
-    ssize_t found = -1;
-    for (size_t i = 0; i < breakpoints->len; i++) {
-        b = (breakpoint_t*)(vec_peek(breakpoints, i));
-        if (b->address == address) {
-            found = i;
-            break;
-        }
-    }
-    return found != -1 ? b : NULL;
-}
-
 static
-i64 breakpoint_write_bin(config_t* cfg, size_t const address, u64 const data)
+i64 breakpoint_write_bin(pid_t const pid, size_t const address, u64 const data)
 {
     errno = 0;
-    u64 peek_data = ptrace(PTRACE_PEEKDATA, cfg->pid, address, NULL);
+    u64 peek_data = ptrace(PTRACE_PEEKDATA, pid, address, NULL);
     if (errno != 0) {
         return -1;
     }
 
     u64 original_data = peek_data & 0xff;
     u64 poke_data = ((peek_data & ~0xff) | (data & 0xff));
-    if (ptrace(PTRACE_POKEDATA, cfg->pid, address, poke_data) < 0) {
+    if (ptrace(PTRACE_POKEDATA, pid, address, poke_data) < 0) {
         return -1;
     }
 
-    return (u64)(original_data);
+    return (i64)(original_data);
 }
 
 static
@@ -67,14 +55,14 @@ i32 breakpoint_enable_address(config_t* cfg, size_t const address)
     if (id < 0) {
         return -1;
     }
-    breakpoint_t* b = vec_peek(cfg->breakpoints, id);
-    if (!b->is_enabled) {
-        i64 original_data = breakpoint_write_bin(cfg, b->address, INT3);
+    breakpoint_t* b = vec_get(cfg->breakpoints, id);
+    if (b->state != BRK_ENABLED) {
+        i64 original_data = breakpoint_write_bin(cfg->pid, b->address, INT3);
         if (original_data < 0) {
             return -1;
         }
-        b->original_data = original_data;
-        b->is_enabled = true;
+        b->original_data = (u64)original_data;
+        b->state = BRK_ENABLED;
     }
 
     return 0;
@@ -91,12 +79,12 @@ i32 breakpoint_disable_address(config_t* cfg, size_t const address)
     if (id < 0) {
         return -1;
     }
-    breakpoint_t* b = vec_peek(cfg->breakpoints, id);
-    if (b->is_enabled) {
-        if (breakpoint_write_bin(cfg, b->address, b->original_data) == -1) {
+    breakpoint_t* b = vec_get(cfg->breakpoints, id);
+    if (b->state != BRK_DISABLED) {
+        if (breakpoint_write_bin(cfg->pid, b->address, b->original_data) < 0) {
             return -1;
         }
-        b->is_enabled = false;
+        b->state = BRK_DISABLED;
     }
 
     return 0;
@@ -104,35 +92,65 @@ i32 breakpoint_disable_address(config_t* cfg, size_t const address)
 
 i32 breakpoint_new(config_t* cfg, size_t const address)
 {
-    if (cfg->state != STATE_RUNNING) {
-        printf("%sinfo:%s debugger is not running\n", MAGENTA, NORMAL);
+    if (cfg->state == STATE_UNINIT) {
+        printf("%s%serror:%s no target executable loaded\n", BOLD, RED, NORMAL);
         return -1;
     }
 
-    if (breakpoint_peek(cfg->breakpoints, address) != NULL) {
+    if (breakpoint_get(cfg->breakpoints, address) != NULL) {
         printf("%sinfo:%s a breakpoint at address 0x%zx already exists\n",
                MAGENTA, NORMAL, address);
         return -1;
     }
 
-    i64 original_data = breakpoint_write_bin(cfg, address, INT3);
-    if (original_data < 0) {
-        return -1;
+    i64 original_data = 0;
+    if (cfg->state == STATE_RUNNING) {
+        original_data = breakpoint_write_bin(cfg->pid, address, INT3);
+        if (original_data < 0) {
+            return -1;
+        }
     }
-
+    
     breakpoint_t self = {
         .address = address,
-        .original_data = original_data,
-        .is_enabled = true,
+        .original_data = (u64)original_data,
+        .state = cfg->state == STATE_RUNNING ? BRK_ENABLED : BRK_UNINIT,
     };
 
     i32 ret = vec_push(cfg->breakpoints, &self);
     if (ret != VEC_OK) {
-        return -1; 
+        return -1;
     }
 
     printf("%sinfo:%s breakpoint #%zu set at address 0x%zx\n", MAGENTA, NORMAL, cfg->breakpoints->len, address);
     return ret;
+}
+
+breakpoint_t const* breakpoint_get(vec_t const* breakpoints, size_t const address)
+{
+    ssize_t id = breakpoint_search(breakpoints, address);
+    if (id < 0) {
+        return NULL;
+    }
+
+    // Safe because unreachable if `breakpoint_search` failed
+    breakpoint_t const* b = vec_peek(breakpoints, (size_t)id);
+    return b;
+}
+
+i32 breakpoint_setup(config_t* cfg)
+{
+    for (size_t i = 0; i < cfg->breakpoints->len; i++) {
+        breakpoint_t* b = vec_get(cfg->breakpoints, i);
+        if (b->state == BRK_UNINIT) {
+            i32 ret = breakpoint_enable_id(cfg, i);
+            if (ret < 0) {
+                return -1;
+            }
+        }
+    }
+
+    return 0;
 }
 
 i32 breakpoint_enable_id(config_t* cfg, size_t const id)
@@ -141,14 +159,14 @@ i32 breakpoint_enable_id(config_t* cfg, size_t const id)
         return -1;
     }
 
-    breakpoint_t* b = vec_peek(cfg->breakpoints, id);
-    if (!b->is_enabled) {
-        i64 original_data = breakpoint_write_bin(cfg, b->address, INT3);
+    breakpoint_t* b = vec_get(cfg->breakpoints, id);
+    if (b->state != BRK_ENABLED) {
+        i64 original_data = breakpoint_write_bin(cfg->pid, b->address, INT3);
         if (original_data < 0) {
             return -1;
         }
         b->original_data = original_data;
-        b->is_enabled = true;
+        b->state = BRK_ENABLED;
     }
 
     return 0;
@@ -160,12 +178,12 @@ i32 breakpoint_disable_id(config_t* cfg, size_t const id)
         return -1;
     }
 
-    breakpoint_t* b = vec_peek(cfg->breakpoints, id);
-    if (b->is_enabled) {
-        if (breakpoint_write_bin(cfg, b->address, b->original_data) == -1) {
+    breakpoint_t* b = vec_get(cfg->breakpoints, id);
+    if (b->state != BRK_DISABLED) {
+        if (breakpoint_write_bin(cfg->pid, b->address, b->original_data) == -1) {
             return -1;
         }
-        b->is_enabled = false;
+        b->state = BRK_DISABLED;
     }
 
     return 0;
@@ -178,15 +196,13 @@ i32 breakpoint_remove_address(config_t* cfg, size_t const address)
         return -1;
     }
 
-    for (size_t i = 0; i < cfg->breakpoints->len; i++) {
-        breakpoint_t* current = vec_peek(cfg->breakpoints, i);
-        if (current->address == address) {
-            return vec_delete(cfg->breakpoints, i) == 0 ? 0 : -1;
-        }
+    ssize_t id = breakpoint_search(cfg->breakpoints, address);
+    if (id < 0) {
+        printf("%swarning:%s no breakpoint set at address 0x%16zx\n", YELLOW, NORMAL, address); 
+        return -1;
     }
 
-    printf("%swarning:%s no breakpoint set at address 0x%16zx\n", YELLOW, NORMAL, address); 
-    return -1;
+    return vec_delete(cfg->breakpoints, id) == VEC_OK ? 0 : -1;
 }
 
 i32 breakpoint_remove_id(config_t* cfg, size_t const id)
@@ -212,7 +228,7 @@ i32 breakpoint_step(config_t* cfg)
     }
 
     u64 address = registers.rip - 1;
-    breakpoint_t* b = breakpoint_peek(cfg->breakpoints, address);
+    breakpoint_t const* b = breakpoint_get(cfg->breakpoints, address);
     if (!b) {
         return 0;
     }
@@ -234,7 +250,7 @@ i32 breakpoint_step(config_t* cfg)
         return -1;
     }
 
-    return 1;
+    return 0;
 }
 
 void breakpoint_list(config_t const* cfg)
@@ -245,8 +261,8 @@ void breakpoint_list(config_t const* cfg)
     }
 
     for (size_t i = 0; i < cfg->breakpoints->len; i++) {
-        breakpoint_t* b = vec_peek(cfg->breakpoints, i);
+        breakpoint_t const* b = vec_peek(cfg->breakpoints, i);
         printf("Breakpoint #%zu at 0x%zx %s\n", i + 1, b->address,
-               b->is_enabled == true ? "\033[32m(enabled)\033[0m" : "\033[31m(disabled)\033[0m");
+               b->state != BRK_DISABLED ? "\033[32m(enabled)\033[0m" : "\033[31m(disabled)\033[0m");
     }
 }
